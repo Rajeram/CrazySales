f896d925afc59ac4ca93a6aad814d35259a12d4a
Rajeram <raam750@gmail.com>
2022-03-28 21:32:12 +0530

    new project added

---
+++ src/org/test/ArrayExample.java
@@ -0,0 +1,45 @@
+package org.test;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+
+		
+//		String s[]=new String[5];
+		
+		//we can also declare as below
+		String s[]={"India","Japan","France","England","Spain"};
+//		
+//		s[0]="India";
+//		s[1]="Japan";
+//		s[2]="France";
+//		s[3]="England";
+//		s[4]="Spain";
+		
+		System.out.println(s[3]);
+		
+		
+		System.out.println(s.length);
+		
+		System.out.println("***********************");
+	
+		//Normal For loop
+	for (int i = 0; i < s.length; i++) {
+		
+		System.out.println(s[i]);
+		
+	}
+	
+	System.out.println("***********************");
+	//Enhanced for Loop
+	for (String x : s) {
+		
+		System.out.println(x);
+	}
+	
+
+	
+	}
+
+}
+
