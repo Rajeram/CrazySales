b95035ab6ce64c9bbb93ee52b9cc64c8f18983e2
Rajeram <raam750@gmail.com>
2022-08-16 10:01:38 +0530

    this is first commit

---
+++ src/org/univ/College.java
@@ -0,0 +1,24 @@
+package org.univ;
+
+public class College  {
+	
+	public void ug() {
+		System.out.println("i will study bsc");
+
+	}
+	public void pg() {
+		System.out.println("i will not study msc");
+
+	}
+	
+
+	public static void main(String[] args) {
+		
+
+		College c=new College();
+		c.ug();
+		c.pg();
+		
+	}
+
+}
---
+++ src/org/univ/University.java
@@ -0,0 +1,23 @@
+package org.univ;
+
+public class University {
+	
+	public static void ug() {
+		System.out.println("i will study bsc");
+
+	}
+	
+	public static void pg() {
+	System.out.println("i will study msc");
+
+	}
+	
+
+	public static void main(String[] args) {
+
+		
+		ug();
+		pg();
+	}
+
+}
